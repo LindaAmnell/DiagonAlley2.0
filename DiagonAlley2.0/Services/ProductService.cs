c9a0dd8ba4f4e2e4c9697f92f9f4b55a94d230fc
LindaAmnell <amnell3@hotmail.com>
2025-12-16 14:45:43 +0100

    create Add product

---
+++ DiagonAlley2.0/Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using DiagonAlley2._0.Models;
+
+namespace DiagonAlley2._0.Services
+{
+    public class ProductService
+    {
+        private readonly MongoDbService _db;
+        private const string Collection = "Products";
+
+        public ProductService(MongoDbService db)
+        {
+            _db = db;
+        }
+
+        public Task<List<Product>> GetAll()
+            => _db.GetAllAsync<Product>(Collection);
+
+        public Task Create(Product product)
+            => _db.CreateAsync(Collection, product);
+
+        public Task Update(Product product)
+            => _db.UpdateAsync(Collection, product.Id!, product);
+
+        public Task Delete(string id)
+            => _db.DeleteAsync<Product>(Collection, id);
+    }
+}
